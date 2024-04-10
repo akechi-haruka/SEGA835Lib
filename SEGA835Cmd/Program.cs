af759704a0d53e66b76457ea57a89b858d8fec96
kyoubate-haruka <46010460+kyoubate-haruka@users.noreply.github.com>
2024-04-10 11:00:43 +0200

    Add project files.

---
+++ SEGA835Cmd/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Haruka.Arcade.SEGA835Cmd {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ SEGA835Lib/VersionInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace Haruka.Arcade.SEGA835Lib {
+
+    /**
+     * Version information about the library.
+     */
+    public class VersionInfo {
+
+        /**
+         * The library name.
+         */
+        public static readonly String LIB_NAME = typeof(VersionInfo).Assembly.GetName().Name;
+
+        /**
+         * The library version.
+         */
+        public static readonly Version LIB_VERSION = typeof(VersionInfo).Assembly.GetName().Version;
+
+    }
+}
