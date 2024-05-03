303fa0ae01f863130b0dba9a88d987afa656915b
kyoubate-haruka <46010460+kyoubate-haruka@users.noreply.github.com>
2024-05-03 15:16:58 +0200

    More docs

--- SEGA835Lib/Devices/Card/837-15396/RadioOnType.cs
+++ SEGA835Lib/Devices/Card/837-15396/RadioOnType.cs
@@ -5,9 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Haruka.Arcade.SEGA835Lib.Devices.Card._837_15396 {
+
+    /// <summary>
+    /// The type for which card(s) the radio should be turned on.
+    /// </summary>
     public enum RadioOnType {
+        /// <summary>
+        /// Turn on radio for MIFARE cards.
+        /// </summary>
         MIFARE = 1,
+        /// <summary>
+        /// Turn on radio for FeliCa cards.
+        /// </summary>
         FeliCa,
+        /// <summary>
+        /// Turn on radio for both cards.
+        /// </summary>
         Both
     }
 }
