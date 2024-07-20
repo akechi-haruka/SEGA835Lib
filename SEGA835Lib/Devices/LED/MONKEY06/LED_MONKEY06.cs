1d0a96e7eaeb5ae8ba6ad997cbbeb4db717dcffe
kyoubate-haruka <46010460+kyoubate-haruka@users.noreply.github.com>
2024-07-20 09:37:56 +0200

    Fix compilation

--- SEGA835Lib/Devices/LED/MONKEY06/LED_MONKEY06.cs
+++ SEGA835Lib/Devices/LED/MONKEY06/LED_MONKEY06.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Haruka.Arcade.SEGA835Lib.Devices.LED.MONKEY06 {
