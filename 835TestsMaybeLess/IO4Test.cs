3332b0bac38657fb7a66d906fb6cab7a573271e7
kyoubate-haruka <46010460+kyoubate-haruka@users.noreply.github.com>
2026-09-09 12:10:01 +0200

    Add test for IO4 capability parsing

--- 835TestsMaybeLess/IO4Test.cs
+++ 835TestsMaybeLess/IO4Test.cs
@@ -1,4 +1,5 @@
 ﻿using Haruka.Arcade.SEGA835Lib.Devices;
+using Haruka.Arcade.SEGA835Lib.Devices.IO;
 using Haruka.Arcade.SEGA835Lib.Devices.IO._835_15257_01;
 
 namespace _835TestsMaybeLess;
@@ -54,5 +55,27 @@ class Io4Test {
         }
 
         Assert.That(io4.ClearLed(), Is.EqualTo(DeviceStatus.Ok));
+    }
+
+    [Test]
+    public void T04_TestProductParsing() {
+        String product = "I/O CONTROL BD;15257;01;90;1831;6679A;00;GOUT=14_ADIN=8,E_ROTIN=4_COININ=2_SWIN=2,E_UQ1=41,6";
+
+        Assert.That(JvsCapabilities.TryParse(product, out JvsCapabilities capabilities), Is.True);
+        Assert.That(capabilities, Is.Not.Null);
+
+        Assert.That(capabilities.Type, Is.EqualTo("I/O CONTROL BD"));
+        Assert.That(capabilities.Outputs, Is.EqualTo(0x14));
+        Assert.That(capabilities.AnalogInputs.Length, Is.EqualTo(8));
+        Assert.That(capabilities.AnalogInputs[0], Is.EqualTo(0xE));
+        Assert.That(capabilities.AnalogInputs[7], Is.EqualTo(0xE));
+        Assert.That(capabilities.RotaryInputs, Is.EqualTo(4));
+        Assert.That(capabilities.Chutes, Is.EqualTo(2));
+        Assert.That(capabilities.SwitchInputs.Length, Is.EqualTo(2));
+        Assert.That(capabilities.SwitchInputs[0], Is.EqualTo(0xE));
+        Assert.That(capabilities.SwitchInputs[1], Is.EqualTo(0xE));
+        Assert.That(capabilities.UniqueFunctions.Length, Is.EqualTo(1));
+        Assert.That(capabilities.UniqueFunctions[0].Id, Is.EqualTo(0x41));
+        Assert.That(capabilities.UniqueFunctions[0].Value, Is.EqualTo(6));
     }
 }
